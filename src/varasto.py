012d77b0415f7333deb6c8e73d9ba86751117ef5
shoyuhe <sofia.heikkinen@icloud.com>
2025-11-02 21:46:23 +0200

    Modify varasto.py

--- src/varasto.py
+++ src/varasto.py
@@ -1,45 +1,20 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
+        self.tilavuus = tilavuus
 
-        if alku_saldo < 0.0:
-            # virheellinen, nollataan
-            self.saldo = 0.0
-        elif alku_saldo <= tilavuus:
-            # mahtuu
-            self.saldo = alku_saldo
-        else:
-            # täyteen ja ylimäärä hukkaan!
-            self.saldo = tilavuus
+        self.saldo = alku_saldo
 
     # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
     def lisaa_varastoon(self, maara):
-        if maara < 0:
-            return
-        if maara <= self.paljonko_mahtuu():
-            self.saldo = self.saldo + maara
-        else:
-            self.saldo = self.tilavuus
+        self.saldo = self.saldo + maara
 
     def ota_varastosta(self, maara):
-        if maara < 0:
-            return 0.0
-        if maara > self.saldo:
-            kaikki_mita_voidaan = self.saldo
-            self.saldo = 0.0
-
-            return kaikki_mita_voidaan
-
         self.saldo = self.saldo - maara
 
         return maara
 
-    def __str__(self):
-        return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
+    # def __str__(self):
+    #     return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
