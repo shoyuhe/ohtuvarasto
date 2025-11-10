79dcc8df1c4e6e665f60adc04354742b38bafba6
shoyuhe <sofia.heikkinen@icloud.com>
2025-11-10 19:40:21 +0200

    Add pylint

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -1,30 +1,38 @@
+"""Importing unittest and varasto"""
 import unittest
 from varasto import Varasto
 
 
 class TestVarasto(unittest.TestCase):
+    """Configure TestVarasto"""
     def setUp(self):
         self.varasto = Varasto(10)
 
     def test_konstruktori_luo_tyhjan_varaston(self):
+        """Create empty storage"""
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
     def test_uudella_varastolla_oikea_tilavuus(self):
+        """Check storage volume"""
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
 
     def test_lisays_lisaa_saldoa(self):
+        """Test add"""
         self.varasto.lisaa_varastoon(8)
 
         self.assertAlmostEqual(self.varasto.saldo, 8)
 
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
+        """Add more"""
         self.varasto.lisaa_varastoon(8)
 
         # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
 
+    #awwwwwwwwwrererererererrerererrererererrerererererrerererrerererererrererererrerererrererererrerererererererere
     def test_ottaminen_palauttaa_oikean_maaran(self):
+        """Take from storage"""
         self.varasto.lisaa_varastoon(8)
 
         saatu_maara = self.varasto.ota_varastosta(2)
@@ -32,6 +40,7 @@ class TestVarasto(unittest.TestCase):
         self.assertAlmostEqual(saatu_maara, 2)
 
     def test_ottaminen_lisaa_tilaa(self):
+        """Take from storage"""
         self.varasto.lisaa_varastoon(8)
 
         self.varasto.ota_varastosta(2)
--- src/varasto.py
+++ src/varasto.py
@@ -1,17 +1,24 @@
+"""Varasto"""
+
 class Varasto:
+    """Define varasto"""
     def __init__(self, tilavuus, alku_saldo = 0):
         self.tilavuus = tilavuus
 
         self.saldo = alku_saldo
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea.
+    # Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
+        """Check how much space"""
         return self.tilavuus - self.saldo
 
     def lisaa_varastoon(self, maara):
+        """Add to storage"""
         self.saldo = self.saldo + maara
 
     def ota_varastosta(self, maara):
+        """Take from storage"""
         self.saldo = self.saldo - maara
         return maara
 
